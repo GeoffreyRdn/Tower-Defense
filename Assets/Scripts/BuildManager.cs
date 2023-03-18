ac91f991d7bf5e330b229a6f8b6c36cb116f2e75
geoffrey.redon <geoffrey.redon@epita.fr>
2023-03-18 16:30:27 -0700

    Tower Defense Part 1 DONE

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+	public static BuildManager instance;
+
+	[SerializeField] private GameObject buildEffect;
+	private TurretBlueprint turretToBuild;
+	
+	public bool CanBuild => turretToBuild != null;
+	public bool HasMoney => PlayerStats.money >= turretToBuild.cost;
+
+	private void Awake()
+	{
+		if (instance != null)
+		{
+			Debug.LogError("ERROR : More than one BuildManager in scene!");
+			return;
+		}
+		
+		instance = this;
+	}
+	
+	public void BuildTurretOn(Node node)
+	{
+		if (PlayerStats.money < turretToBuild.cost)
+		{
+			Debug.Log("Not enough money to build that!");
+			return;
+		}
+
+		// buy and instantiate the turret on the current node
+		
+		PlayerStats.money -= turretToBuild.cost;
+
+		GameObject turret = Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
+		node.turret = turret;
+
+		GameObject effect = Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+		Destroy(effect, 5f);
+
+		Debug.Log("Turret build! Money left: " + PlayerStats.money);
+	}
+
+	public void SelectTurretToBuild (TurretBlueprint turret)
+		=> turretToBuild = turret;
+}
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,76 @@
+using System.Linq;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	
+	[SerializeField] private float speed = 70f;
+	[SerializeField] private int damage = 50;
+	[SerializeField] private float explosionRadius = 0f;
+	[SerializeField] private GameObject impactEffect;
+	
+	private Transform target;
+	
+	public void Seek(Transform target)
+		=> this.target = target;
+	
+	private void Update() 
+	{
+		// no more target , destroy the bullet
+		if (target == null)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		// get the enemy position to hit
+		Vector3 dir = target.position - transform.position;
+		var distanceThisFrame = speed * Time.deltaTime;
+
+		// hit
+		if (dir.magnitude <= distanceThisFrame)
+		{
+			HitTarget();
+			return;
+		}
+
+		// move the bullet
+		transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+		transform.LookAt(target);
+	}
+
+	private void HitTarget()
+	{
+		GameObject effectIns = Instantiate(impactEffect, transform.position, transform.rotation);
+		Destroy(effectIns, 5f);
+
+		// check if we put in the editor an explosion
+		if (explosionRadius > 0f) Explode();
+		else Damage(target);
+
+		Destroy(gameObject);
+	}
+
+	private void Explode()
+	{
+		// do damage to each enemy in the radius of our explosionRadius
+		
+		var colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+
+		foreach (Collider col in colliders)
+		{
+			if (col.CompareTag($"Enemy")) Damage(col.transform);
+		}
+	}
+
+	private void Damage (Transform enemy)
+	{
+		Enemy e = enemy.GetComponent<Enemy>();
+		if (e != null) e.TakeDamage(damage);
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, explosionRadius);
+	}
+}
